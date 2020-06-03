6e14f9c5ecddf1950c89fb91f6eee45d44da4dea
pk1097816502 <1097816502@qq.com>
2020-06-03 11:58:50 +0800

    新增vuex

--- App.vue
+++ App.vue
@@ -1,13 +1,42 @@
 <script>
 	export default {
 		onLaunch: function() {
-			console.log('App Launch')
+			console.log('App Launch');
+			
+			// #ifdef MP-WEIXIN
+				this.getCode((res)=>{
+					console.log("wx.login",res);
+				});
+			// #endif
 		},
 		onShow: function() {
 			console.log('App Show')
 		},
 		onHide: function() {
 			console.log('App Hide')
+		},
+		methods:{
+			getCode(fun,provider=""){
+				if(provider==""){
+					uni.login({
+						success:(res) => {
+							console.log("uni.login",res);
+							if(typeof fun == "function"){
+								fun(res);
+							}
+						}
+					})
+				}else{
+					uni.login({
+						provider:provider,
+						success:(res) => {
+							if(typeof fun == "function"){
+								fun(res);
+							}
+						}
+					})
+				}
+			}
 		}
 	}
 </script>
